c7c9c70fd40e68b811d73684dbc7de2a25254689
tnine1 <byiringirofiston18@gmail.com>
2026-02-25 13:08:00 +0200

    Update EmailJS initialization and service/template IDs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function(){
-  emailjs.init("P8F7fV4pfXdmp2PEF");
+  emailjs.init("-4eouI2L2uarn3Eiz");
 })();
 
 const params = new URLSearchParams(window.location.search);
@@ -13,7 +13,7 @@ if(table){
 
   statusText.innerHTML = "Notifying staff...";
   
-  emailjs.send("service_vfde9p3","Hellomyguy0005252",{
+  emailjs.send("service_s88acd7","template_io3n30m",{
     table_number: table,
     time: new Date().toLocaleString()
   }).then(function(response){
